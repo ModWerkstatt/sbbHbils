49d48f65a2bda126612375804c2e80e87c368955
Florian Thiel <flth@flth.de>
2023-02-26 13:03:11 +0100

    Modnamen geändert, Tooltips hinzugefügt

--- strings.lua
+++ strings.lua
@@ -2,30 +2,35 @@ function data()
 return {
 	en = {
 
-		["mod_name"] = "SBB 2-axle sliding wall car (Hbi(l)s)",
+		["mod_name"] = "Sliding wall wagon SBB with 2-axles (Hbi(l)s)",
 		["mod_desc"] = "In December 1968, SBB placed an order with the Swiss wagon industry and ordered 100 wagons from Ateliers de constructions mécaniques de Vevey ACMV. Delivery of these wagons began in July 1970, with up to 20 wagons leaving the Vevey workshops each month. By 1981, a total of 3351 Hbis of the ACMV type were in service with SBB. In 1972, some variants with insulated sliding walls were provided. Over the years, the cars were also used as advertising vehicles and could be seen in numerous paint schemes. All of the advertising paint schemes existed in real life.",
 		
         ["SBBHbils_desc"] = "In 1968, Ateliers de constructions mécaniques de Vevey ACMV developed standard sliding wall cars for SBB. By 1981, a total of 3351 Hbis of the ACMV type were in service with SBB. Over the years, there were several variants and extensions of these cars. These are still in service today.",
 		["SBBHbilsGlatt_desc"] = "In 1973, some of the Hbis were equipped with insulated sliding walls (electric heating and thermal insulation) and classified as Hbis-vxy. These existed and exist in numerous advertising paint schemes and are still in use today.",
 	    ["sound_check"] = "Activate custom sound?",
 		["height"] = "Height",
+		["height_tooltip"] = "Configure height",
+		["jrm_param_offsetx_tooltip"] = "Configure offset",
+		["jrm_param_type_tooltip"] = "Type",
 		["offset x-axis"] = "Offset x-axis",
 		["ground"] = "Ground",
 		["rail"] = "Rail",
 		["vehicle"] = "Vehicle",
 
-		
 	},
 
 	de = {
 
-		["mod_name"] = "SBB 2-Achser Schiebewandwagen (Hbi(l)s)",
+		["mod_name"] = "Schiebewandwagen SBB 2-achsig (Hbi(l)s)",
 		["mod_desc"] = "Im Dezember 1968 tätigte die SBB eine Bestellung bei der Schweizerischen Wagenindustrie und orderte bei Ateliers de constructions mécaniques de Vevey ACMV 100 Wagen. Ab Juli 1970 begann die Auslieferung dieser Wagen, wobei bis zu 20 Wagen pro Monat die Werkshallen in Vevey verliessen. Bis 1981 waren insgesamt 3351 Hbis vom Typ-ACMV bei der SBB im Einsatz. 1972 wurden einige Varianten mit isolierten Schiebewänden bereitgestellt. Im Laufe der Jahre wurden die Wagen auch als Werbeträger genutzt und waren in zahlreichen Lackierungen zu beobachten. Alle enthalten Werbelackierungen entsprechen realen Vorbildern.",
 		
         ["SBBHbils_desc"] = "1968 wurden von Ateliers de constructions mécaniques de Vevey ACMV Standard Schiebewandwagen für die SBB entwickelt. Bis 1981 waren insgesamt 3351 Hbis vom Typ-ACMV bei der SBB im Einsatz. Im Laufe der Jahre gab es einige Varianten und Erweiterungen dieser Wagen. Diese sind bis heute weiterhin im Einsatz.",
 		["SBBHbilsGlatt_desc"] = "Im Jahre 1973 wurden einige der Hbis mit isolierten Schiebewänden (elektrischer Heizung und Wärmeisolation) ausgestattet und als Hbis-vxy eingereiht. Diese gab und gibt es in zahlreichen Werbelackierungen und sind bis heute im Einsatz. ",
 		["sound_check"] = "Eigenes Soundset aktivieren?",
 		["height"] = "Höhe",
+		["height_tooltip"] = "Höhe einstellen",
+		["jrm_param_offsetx_tooltip"] = "Versatz einstellen",
+		["jrm_param_type_tooltip"] = "Typ",
 		["offset x-axis"] = "Verschub X-Achse",
 		["ground"] = "Boden",
 		["rail"] = "Gleis",
